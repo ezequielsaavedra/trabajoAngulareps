035106d1d449c184475765445cfab772e6a8c121
ezequiel <saavedraeze66@gmail.com>
2023-02-09 14:03:55 -0300

    directiva personalizada

--- src/app/directives/titulos.directive.ts
+++ src/app/directives/titulos.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appTitulos]'
 })
 export class TitulosDirective {
 
-  constructor() { }
+  constructor(
+    private element: ElementRef,
+    private render: Renderer2
+  ) { 
+    this.render.setStyle(this.element.nativeElement, "font-size", "20px")
+  }
 
 }
